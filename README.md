a73cabdd25da28bbf214a9a2d79c8c71cb66310c
oksr <ofekn271@gmail.com>
2022-05-10 15:34:33 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # nexrender-action-slack-alert
 Utility module for sending a Slack alert when render start/finish or render error.
+
+
+## Installation
+
+Install the module via Git :
+```
+npm i -g https://github.com/oksr/nexrender-action-slack-alert
+```
+
+## Usage
+
+The output can be relative or absolute. If it's relative, the workpath will be added.
+If the output is not specified, the filename "encoded.mp4" will be used.
+
+You can add the 'debug' option to see the progress of handbrake processing. (default: false)
+
+The 'eraseInput' option will erase the original video input after the processing. (default: false)
+
+```
+actions:{
+    postrender:[{
+        module: "nexrender-action-slack-alert",
+        conversationId: "C011UTKEV1N",
+        text: "optional text"
+    }]
+},
+```
---
+++ index.js
@@ -0,0 +1,128 @@
+const { WebClient } = require("@slack/web-api");
+const token = process.env.SLACK_TOKEN;
+
+module.exports = (job, settings, action, type) => {
+  console.log(job);
+  
+  const slackWeb = new WebClient(token);
+  const icon_emoji = type === "prerender" ? ":robot_face:" : ":white_check_mark:";
+  const attachments = getAttachments(job, type);
+
+  return new Promise((resolve, reject) => {
+    slackWeb.chat
+      .postMessage({
+        channel: action.conversationId,
+        username: "Render Bot",
+        text: "New alert",
+        icon_emoji: icon_emoji,
+        attachments: attachments,
+      })
+      .then((res) => {
+        console.log(
+          `Successfully send message ${res.ts} in conversation ${action.conversationId}`
+        );
+        resolve(job);
+      })
+      .catch((e) => {
+        console.log(`*** slack action error: ${e.data.error} ***`);
+        reject(e);
+      });
+  });
+};
+
+
+// Here you can edit the attachment send to slack as message
+function getAttachments(job, type) {
+  if (type === "prerender") {
+    return [
+      {
+        blocks: [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: "Render start 🚀",
+              emoji: true,
+            },
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "Job Details:",
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Job uid:*\n ${job.uid}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*Composition:*\n ${job.template.composition}`,
+              },
+            ],
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Template src:*\n ${job.template.src}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*Output:*\n ${job.output}`,
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  } else {
+    return [
+      {
+        blocks: [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: "Render Finished ✅",
+              emoji: true,
+            },
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "Job Details:",
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Job uid:*\n ${job.uid}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*Output:*\n ${job.output}`,
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
+}
